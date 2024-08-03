e5871fa4bfa7c23ad6cff155015e17efae54fcf8
Nageshwar Tripathi <stpandstp.martialstylus@gmail.com>
2024-08-03 23:34:15 +0530

    created: registration-page

--- main/views.py
+++ main/views.py
@@ -1,11 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import User
+from django.contrib import messages
 
 def home(request):
     return render(request, 'main/home.html')
 
 
 def register(request):
-    return render(request, 'main/register.html')
+    if request.method == 'POST':
+        
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        # validation for unique email
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'Email already exists')
+            return render(request, 'main/register.html')
+        
+        new_user = User(
+            name=name,
+            email=email,
+            password=password
+        )
+        new_user.save()
+
+        return redirect('/')
+
+    else:
+        return render(request, 'main/register.html')
 
 
 def login(request):
