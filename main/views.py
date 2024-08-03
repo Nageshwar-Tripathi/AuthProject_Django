c58c9bba6287e6c7088be125381097041491612c
Nageshwar Tripathi <stpandstp.martialstylus@gmail.com>
2024-08-03 22:56:32 +0530

    added: static-and-templtes

--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'main/home.html')
+
+
+def register(request):
+    return render(request, 'main/register.html')
+
+
+def login(request):
+    return render(request, 'main/login.html')
