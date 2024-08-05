25bdfa4560dcd2a1ad61e5bedfa2af53184e4d13
Nageshwar Tripathi <stpandstp.martialstylus@gmail.com>
2024-08-05 23:04:32 +0530

    enabled: session-management

--- main/views.py
+++ main/views.py
@@ -4,50 +4,61 @@ from .models import User
 from django.contrib import messages
 
 def home(request):
-    email = ""
-    data = {
-        'email': email
-    }
-    return render(request, 'main/home.html',data)
+
+    if request.session.has_key('logged'):
+        email = request.session['logged']
+        data = {
+            'email': email
+        }
+        return render(request, 'main/home.html',data)
+    else:
+        return redirect('/login/')
 
 
 def register(request):
-    if request.method == 'POST':
-        
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-
-        # validation for unique email
-
-        if User.objects.filter(email=email).exists():
-            messages.error(request, 'Email already exists')
-            return render(request, 'main/register.html')
-        
-        new_user = User(
-            name=name,
-            email=email,
-            password=password
-        )
-        new_user.save()
-
+    if request.session.has_key('logged'):
         return redirect('/')
-
     else:
-        return render(request, 'main/register.html')
+        if request.method == 'POST':
+            
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+
+            # validation for unique email
+
+            if User.objects.filter(email=email).exists():
+                messages.error(request, 'Email already exists')
+                return render(request, 'main/register.html')
+            
+            new_user = User(
+                name=name,
+                email=email,
+                password=password
+            )
+            new_user.save()
+            request.session['logged'] = email
+            return redirect('/')
+
+        else:
+            return render(request, 'main/register.html')
 
 
 def login(request):
-    if request.method == 'POST':
-        
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-
-        if User.objects.filter(email=email, password=password).exists():
-            return redirect('/')
-        else:
-            messages.error(request, "Email or/and Password is incorrect")
-            return render(request, 'main/login.html')
-
+    if request.session.has_key('logged'):
+        return redirect('/')
     else:
-        return render(request, 'main/login.html')
+        if request.method == 'POST':
+            
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+
+            if User.objects.filter(email=email, password=password).exists():
+                request.session['logged'] = email
+                return redirect('/')
+            else:
+                messages.error(request, "Email or/and Password is incorrect")
+                return render(request, 'main/login.html')
+
+        else:
+            return render(request, 'main/login.html')
