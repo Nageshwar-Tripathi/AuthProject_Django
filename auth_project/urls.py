d031b591460fe370d6574ffaf4be9962b2b87b7f
Nageshwar Tripathi <stpandstp.martialstylus@gmail.com>
2024-08-06 21:41:02 +0530

    added: admin-headers

--- auth_project/urls.py
+++ auth_project/urls.py
@@ -2,6 +2,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Auth Project"
+admin.site.site_title = "Auth Project"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
